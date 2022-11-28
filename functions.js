b8f03e4f9bdc4674baa33ceeb5ec17928ae0c7cc
Sandrita <sandramilenagarciaa@gmail.com>
2022-11-28 15:49:00 -0800

    fucntions complete

--- functions.js
+++ functions.js
@@ -1,13 +1,33 @@
-'use strict';
-
+"use strict";
 // 1. isHometown
-
-// Define your function here
+const isHometown = (town) => town === "San Francisco";
+console.log(isHometown("San Francisco"));
 
 // 2. getFullName
 
-// Define your function here
+const getFullName = (firstName, lastName) => {
+  return `${firstName} ${lastName}`;
+};
+
+console.log(getFullName("sandra", "N"));
 
 // 3. calculateTotal
 
-// Define your function here
+const calculateTotal = (base_price, state, tax = 0.05) => {
+  let subtotal = base_price * (1 + tax);
+  let fee = 0;
+
+  if (state === "CA") {
+    fee == 0.03 * subtotal;
+  } else if (state === "PA") {
+    fee = 2;
+  } else if (state === "MA") {
+    if (base_price <= 100) {
+      fee = 1;
+    } else {
+      fee = 3;
+    }
+  }
+  return subtotal + fee;
+};
+console.log(calculateTotal(8, "CA", 0.05));
